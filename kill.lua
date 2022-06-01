0c753470729ffcd8e5a28c9ba06c2792370339ee
keegang6705 <75747783+keegang6705@users.noreply.github.com>
2022-06-01 08:52:15 +0700

    Update kill.lua

--- kill.lua
+++ kill.lua
@@ -1,4 +1,4 @@
-local character = game.Players.name.Character
+local character = game.Players.name.Character -- or game.Players.LocalPlayer.Character
 local humanoid = character:FindfirstChildOfClass("Humanoid")
 if humanoid then
   humanoid.Health = 0
